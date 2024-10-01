2fef544800c6d5123acdd7d89ff0ddef87988e7a
Alexey Vanyukov <a_vanyukov@bk.ru>
2024-10-02 00:48:55 +0500

    ajax request for brackets

--- function/brackets.php
+++ function/brackets.php
@@ -1,7 +1,9 @@
 <?php
 
-function brackets($string)
-{
+include "../database/dbconnect.php";
+
+function brackets($string){
+
     //Создаем стек
     $stack = [];
     //Создаем список открывающих скобок
@@ -33,12 +35,43 @@ function brackets($string)
             //Проверяем,есть ли такая пара в списке пар
             if (!in_array($pair, $pairs))
             {
-                return false;
+                return json_encode(false);
             }
         }
     }
     //Если стек пустой, возвращает true
-    return count($stack) === 0;
+    return json_encode(true);
 
 }
+
+$msg_box = ""; // Храним сообщение для вывода
+$errors = array(); // Массив для ошибков
+
+// Проверяем поля
+if($_POST['string'] == "")    $errors[] = "Поле 'Ваша строка' не заполнено!";
+
+// если форма без ошибок
+if(empty($errors)){
+    // собираем данные из формы
+    $string  = $_POST['string'];
+    $result = brackets($string);
+    $sql = "INSERT INTO history (string,status) values ('$string','$result')";
+    mysqli_query($connection, $sql);
+    $msg_box = $result;
+}else{
+    // если были ошибки, то выводим их
+    $msg_box = "";
+    foreach($errors as $one_error){
+        $msg_box = $one_error;
+    }
+}
+
+// Делаем ответ в формате json
+echo json_encode(array(
+    'success' => $msg_box
+));
+
+
+
+
 
