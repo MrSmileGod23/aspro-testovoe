e3a88ac7417bc1f341ebd45e01167eb0eec53b72
Alexey Vanyukov <a_vanyukov@bk.ru>
2024-10-01 20:50:00 +0500

    add show table with ajax, code style refactor

---
+++ database/dbconnect.php
@@ -0,0 +1,8 @@
+<?php
+$database_host = "localhost";
+$database_user = "root";
+$database_password = "";
+$database_name = "History";
+
+$connection = mysqli_connect($database_host, $database_user, $database_password, $database_name);
+
